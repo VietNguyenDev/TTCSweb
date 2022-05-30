b8b34a39b8a2303531623823d8d671a790ccaddc
VietNguyenDev <nguyenviet25102001@gmail.com>
2022-05-30 22:37:57 +0700

    comment

--- app/http/services/order.service.js
+++ app/http/services/order.service.js
@@ -1,13 +1,2 @@
-import dayjs from "dayjs";
-async function createOrderDetailsService(orderItems,userId) {
-    const order = {
-        create_at : dayjs().format('YYYY-MM-DD'),
-        user_id: userId,
-        orderItems: orderItems.map((element) => ({
-            product_id: element.product_id,
-            quantity: element.quantity,
-        })),
-    };
-};
 
 export default createOrderDetailsService;
